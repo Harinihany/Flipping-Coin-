9a90f5694edcdb083c1cdb85f0f454fee8a86022
Harinihany <harinihany03@gmail.com>
2020-11-20 20:58:51 +0530

    added: UC-3

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -35,4 +35,26 @@ then
 
 fi
 done
+echo ************
+headcount=0
+tailcount=0
+temp=0
+while(($temp!=1))
+do
+random=$(( $RANDOM % 2 ))
+if(( $random == 1 ))
+then
+        echo " winner is heads"
+        ((headcount++))
+else
+        echo " winner is tails"
+        ((tailcount++))
+fi
+echo "tail count is $tailcount"
+echo "head count is $headcount"
+if (( $headcount == 21 ))
+then
+        temp=1
 
+fi
+done
